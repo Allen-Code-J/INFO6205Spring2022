fb4c6a9a1e17ef83639f2e625c674deb032d9eb7
ChongyaJin <352577661@qq.com>
2022-02-15 10:26:06 -0800

    committing and pushing

--- assignment3/src/leetCode138_CopyListwithRandomPointer.java
+++ assignment3/src/leetCode138_CopyListwithRandomPointer.java
@@ -1,5 +1,5 @@
 /*
-// Definition for a Node.
+Definition for a Node.
 class Node {
     int val;
     Node next;
@@ -15,20 +15,20 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if(head == null){
+        if (head == null) {
             return null;
         }
         Node cur = head;
-        HashMap<Node,Node> map = new HashMap<>();
-        while(cur!=null){
-            map.put(cur,new Node(cur.val));
-            cur = cur.next;
+        HashMap<Node, Node> map = new HashMap<>();
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = ccnext;
         }
-        cur=head;
-        while(cur!=null){
-            map.get(cur).next=map.get(cur.next);
-            map.get(cur).random=map.get(cur.random);
-            cur=cur.next;
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
         }
         return map.get(head);
     }
